033424d2ea9dd593186cc599ebc8c775a0c016e5
Lucas Le <lucas.lpnl@byteuptech.com>
2026-03-09 21:53:05 +0700

    change icon launcher

--- app/src/main/java/com/goriant/jidelite/ui/main/MainScreen.kt
+++ app/src/main/java/com/goriant/jidelite/ui/main/MainScreen.kt
@@ -31,6 +31,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.widthIn
 import androidx.compose.foundation.lazy.LazyColumn
@@ -394,6 +395,7 @@ private fun TopBar(
     Column(
         modifier = Modifier
             .fillMaxWidth()
+            .statusBarsPadding()
             .background(TopBarSurface)
     ) {
         Row(
